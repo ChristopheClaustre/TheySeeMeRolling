d915958339fef15895cf114cd6d5cca0a885ace9
ChristopheClaustre <christophe.claustre@gmail.com>
2017-02-15 22:58:54 +0100

    modification du main de test_audio pour mieux tester la spatialisation

--- src/Audio/main.cxx
+++ src/Audio/main.cxx
@@ -1,7 +1,8 @@
 #include <iostream>
+#include <chrono>
+#include <thread>
 
-//#include <AL/al.h>
-//#include <AL/alc.h>
+#include <AL/al.h>
 
 #include "openal.h"
 #include "wave.h"
@@ -13,15 +14,33 @@ int main(int argc, char *argv[])
     Audio::OpenAL::getInstance()->initialize(listenerPos);
 
     //Loading of the WAVE file
-    Audio::Wave wave("TheySeeMeRolling-Short.wav");
+    Audio::Wave wave("TheySeeMeRolling-Short-mono.wav");
     wave.load();
     wave.createBuffer();
 
     Audio::Source source(wave.getIdBuffer());
+    source.setGain(0.3);
     source.play();
 
-    int truc;
-    std::cin >> truc;
+    ALfloat position[] = {-4.0, 0.0, 1.0};
+
+    float delta = 0.5;
+    std::chrono::milliseconds timespan(int(100));
+//    alSourcef(source.getIdSource(), AL_REFERENCE_DISTANCE, 1.5);
+//    alSourcef(source.getIdSource(), AL_MAX_DISTANCE, 2.5);
+//    alSourcei(source.getIdSource(), AL_SOURCE_TYPE, AL_STREAMING);
+
+    for (float i = -20; i < 20; i+=delta) {
+        position[0] = i;
+        source.setPosition(position);
+        std::this_thread::sleep_for(timespan);
+    }
+
+    while(source.getState() == AL_PLAYING) {
+        std::this_thread::sleep_for(timespan);
+    }
+
+    std::cout << "Done !" << std::endl;
 
     delete Audio::OpenAL::getInstance();
 
